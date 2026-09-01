627ba975c349d20757d38c347d3ac3e2d56c67e8
Chirag Chauhan <official.chirag373@gmail.com>
2026-09-01 19:53:21 +0530

    LeetCode 585 SQL

---
+++ SQL/Investments_in_2016.sql
@@ -0,0 +1,13 @@
+Select Round(Sum(tiv_2016),2) as tiv_2016
+from Insurance
+where tiv_2015 in (
+    Select tiv_2015
+    from Insurance
+    group by tiv_2015
+    having count(*) > 1
+) and (lat, lon) in (
+    Select lat, lon
+    from Insurance
+    group by lat, lon
+    having count(*) = 1
+)
